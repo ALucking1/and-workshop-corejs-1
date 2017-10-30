6b44ed2bb7ec65b0c16321d422b680b9023ed031
Anna Lucking <alucking@and.digital>
2017-10-30 15:42:43 +0000

    functions tests - last test unfinished

--- 07_functions.test.js
+++ 07_functions.test.js
@@ -5,7 +5,7 @@ describe('About Functions', function() {
       return a + b;
     }
 
-    expect(add(1, 2)).toBe(/* 🤔 */);
+    expect(add(1, 2)).toBe(3);
   });
 
   it('should know internal variables override outer variables', function() {
@@ -20,9 +20,9 @@ describe('About Functions', function() {
       return message;
     }
 
-    expect(getMessage()).toBe(/* 🤔 */);
-    expect(overrideMessage()).toBe(/* 🤔 */);
-    expect(message).toBe(/* 🤔 */);
+    expect(getMessage()).toBe('Outer');
+    expect(overrideMessage()).toBe('Inner');
+    expect(message).toBe('Outer');
   });
 
   it('should have lexical scoping', function() {
@@ -34,7 +34,7 @@ describe('About Functions', function() {
       }
       return childfunction();
     }
-    expect(parentfunction()).toBe(/* 🤔 */);
+    expect(parentfunction()).toBe('local');
   });
 
   it('should use lexical scoping to synthesise functions', function() {
@@ -48,31 +48,43 @@ describe('About Functions', function() {
     var mysteryFunction3 = makeMysteryFunction(3);
     var mysteryFunction5 = makeMysteryFunction(5);
 
-    expect(mysteryFunction3(10) + mysteryFunction5(5)).toBe(/* 🤔 */);
+    expect(mysteryFunction3(10) + mysteryFunction5(5)).toBe(23);
   });
+
+  // Example of closure:
+  //  function convert(currency) {
+  //    const rate = request('http://sercis', currency)
+  //    return function(amount) {
+  //      return amount * rate;
+  //    }
+  //  }
+  //
+  // const convertEuroFrom = convert('EUR')
+  //
+  //  convertEuroFrom(1000);
 
   it('should allow extra function arguments', function() {
     function returnFirstArg(firstArg) {
       return firstArg;
     }
 
-    expect(returnFirstArg('first', 'second', 'third')).toBe(/* 🤔 */);
+    expect(returnFirstArg('first', 'second', 'third')).toBe('first');
 
     function returnSecondArg(firstArg, secondArg) {
       return secondArg;
     }
 
-    expect(returnSecondArg('only give first arg')).toBe(/* 🤔 */);
+    expect(returnSecondArg('only give first arg')).toBe(undefined);
 
     function returnAllArgs() {
       var argsArray = [];
       for (var i = 0; i < arguments.length; i += 1) {
         argsArray.push(arguments[i]);
       }
-      return argsArray.join(',');
+      return argsArray.join(', ');
     }
 
-    expect(returnAllArgs('first', 'second', 'third')).toBe(/* 🤔 */);
+    expect(returnAllArgs('first', 'second', 'third')).toBe('first, second, third');
   });
 
   it('should pass functions as values', function() {
@@ -85,28 +97,41 @@ describe('About Functions', function() {
     };
 
     var praiseSinger = { givePraise: appendRules };
-    expect(praiseSinger.givePraise('John')).toBe(/* 🤔 */);
+    expect(praiseSinger.givePraise('John')).toBe('John rules!');
 
     praiseSinger.givePraise = appendDoubleRules;
-    expect(praiseSinger.givePraise('Mary')).toBe(/* 🤔 */);
+    expect(praiseSinger.givePraise('Mary')).toBe('Mary totally rules!');
   });
 
   it('should return a reversed string', () => {
-    function reverse() {}
+    function reverse(str) {
+      const splitString = str.split('');
+      const reversedStrArray = splitString.reverse();
+      const reversedString = reversedStrArray.join('');
+
+      return reversedString;
+    }
 
     expect(reverse('hello')).toEqual('olleh');
   });
 
-  it('should return a human age in dog age (1 1 human year to 7 dog years', () => {
-    function puppyCalculator() {}
+  it('should return a human age in dog age (1 human year to 7 dog years', () => {
+    function puppyCalculator(dogAge) {
+      return Math.floor(dogAge / 7);
+    }
 
     expect(puppyCalculator(35)).toBe(5);
-    expect(puppyCalculator(6)).toBe(1);
+    expect(puppyCalculator(6)).toBe(0);
     expect(puppyCalculator(89)).toBe(12);
   });
 
   it('should return a string with a defined suffix', () => {
-    function addSuffix() {}
+    function addSuffix(suffix) {
+      const newStr = function stringToAddTo(str) {
+        return str + suffix;
+      }
+      return newStr;
+    }
 
     expect(markTaskDone('task1')).toEqual('task1 done!');
     expect(markTaskDone('task2')).toEqual('task2 done!');
