4c7cde3a5bce49001843f3b5e7d59abe37ec8f1b
Anna Lucking <alucking@and.digital>
2017-10-30 10:34:15 +0000

    fix failing tests

--- 01_variables.test.js
+++ 01_variables.test.js
@@ -1,19 +1,19 @@
 test('VAR', () => {
   var x = 5;
-  var x = 6;
+  var y = 6;
   expect(x).toBe(5);
 });
 
 test('LET and VAR', () => {
-  var x = 6;
+  // var x = 6;
   let x = 5;
   expect(x).toBe(5);
 });
 
 test('LET', () => {
   let x = 5;
-  let x = 6;
-  expect(x).toBe(6);
+  let y = 6;
+  expect(y).toBe(6);
 });
 
 test('LET', () => {
@@ -25,26 +25,25 @@ test('LET', () => {
     return x;
   }
 
-  expect(x).toBe(20);
+  expect(x).toBe(5);
 });
 
 test('CONST - scalar values', () => {
   const x = 5;
-  x = 'foo';
+  // x = 'foo';
   expect(x).toBe(5);
 });
 
 test('CONST - assignment', () => {
-  const x;
-  x = 5
+  const x = 5;
   expect(x).toBe(5);
 });
 
 test('CONST - objects', () => {
   const person = {
     "name": "Linus",
-    "age": 42
+    "age": 42,
   };
-  
+  person.lastname = 'torvalds'
   expect(person.lastname).toBe('torvalds');
 });
